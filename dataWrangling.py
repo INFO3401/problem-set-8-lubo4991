7396d67a75dfb33b829e0f2a7b0260f4cd0bbd71
lubo4991 <lubo4991@colorado.edu>
2018-10-28 14:07:04 -0600

    Update dataWrangling.py

--- dataWrangling.py
+++ dataWrangling.py
@@ -1,18 +1,24 @@
+Lucas Bouchard
+
+
+
+
 import string
 import os
 from os import listdir
 import csv
-import json
 
 
+https://stackoverflow.com/questions/17176542/remove-specific-character-from-a-csv-file-and-rewrite-to-a-new-file
 
-
-def generateCleanFile(cleaner, targetfile):
-    input_file = open('DesktopData.csv', 'r')
-    output_file = open('fixformat.csv', 'w')
-    data = csv.reader(input_file)
-    writer = csv.writer(output_file)
-    specials = ['<', '>', ]
+def generateCleanFile(input_file, output_file):
+    old_file = open(input_file, 'r')
+    new_file open(output_file, 'w')
+    #input_file = open('DesktopData.csv', 'r')
+    #output_file = open('fixformat.csv', 'w')
+    data = csv.reader(old_file)
+    writer = csv.writer(new_file)
+    specials = ['<', '>']
 
 for line in data:
     line = str(line)
@@ -21,3 +27,5 @@ for line in data:
 
 input_file.close()
 output_file.close()
+
+generateCleanFile("dd-comment-profile.csv", "cleaned-dd-comment-profile.csv")
